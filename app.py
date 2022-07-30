883a887c9e7fd9dfe9f834d9dd3ecde4f22be657
payal9876 <payalyadav9456444@gmail.com>
2022-07-30 12:27:11 +0530

    second commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+app=Flask(__name__)
+
+
+@app.route('/',methods=['POST','GET'])
+def index():
+    return "ci cd pipeline has been established"
+
+
+if __name__=="__main__":
+    app.run(debug=True)
